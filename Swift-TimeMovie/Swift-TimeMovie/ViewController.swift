fa9a736f43bb78d3b8c2564dac5a99779e2333d0
李林泉 <15665292961@163.com>
2016-10-13 14:01:52 +0800

    创建项目

---
+++ Swift-TimeMovie/Swift-TimeMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-TimeMovie
+//
+//  Created by DahaiZhang on 16/10/13.
+//  Copyright © 2016年 LLQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
